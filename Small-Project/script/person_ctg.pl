7933591aba113cbd48fbf61d8c6c8aa34b2ede6a
Faldict <legadoubledyan@gmail.com>
2017-02-23 22:51:42 +0800

    check flag

--- Small-Project/script/person_ctg.pl
+++ Small-Project/script/person_ctg.pl
@@ -9,16 +9,12 @@ open(FILE, "< $INFO_DIR")
 	or die "Couldn't open the files for $!";
 while(<FILE>) {
 	chomp;
-	($filename, $number) = split / +/;
+	($filename, $flag) = split / +/;
     $filename .= ".jpg";
-    # print "Image: $filename, Value: $number \n";
-	# if(int($value) > 0){
-    #    print "Processing image $filename \n";
-	# 	$cmd = "mv " . $IMG_DIR . $filename . " " . $MAIN_DIR . $filename;
-	# 	system($cmd); 
-	# }
-    $cmd = "cp " . $IMG_DIR . $filename . " " . $MAIN_DIR . $filename;
-    system($cmd);
-    print "Processing Image $filename\n";
+	if(int($flag) > 0){
+       print "Processing image $filename \n";
+		$cmd = "cp " . $IMG_DIR . $filename . " " . $MAIN_DIR . $filename;
+		system($cmd); 
+	}
 }
 close FILE;
