a3d293dec43c1b989a62434d8c9c5546d6915031
Faldict <legadoubledyan@gmail.com>
2017-02-25 23:09:05 +0800

    write the number in a new file

--- Small-Project/script/person_ctg.pl
+++ Small-Project/script/person_ctg.pl
@@ -4,17 +4,21 @@
 $INFO_DIR="./ImageSets/Main/person_train.txt";
 $IMG_DIR="./JPEGImages/";
 $MAIN_DIR="./image/";
+$LIST_DIR="person_category.txt";
 
 open(FILE, "< $INFO_DIR")
 	or die "Couldn't open the files for $!";
+open(LIST, "> $LIST_DIR")
 while(<FILE>) {
 	chomp;
 	($filename, $flag) = split / +/;
     $filename .= ".jpg";
 	if(int($flag) > 0){
        print "Processing image $filename \n";
-		$cmd = "cp " . $IMG_DIR . $filename . " " . $MAIN_DIR . $filename;
-		system($cmd); 
+	   $cmd = "cp " . $IMG_DIR . $filename . " " . $MAIN_DIR . $filename;
+	   system($cmd);
+       print LIST "$filename\n"; 
 	}
 }
 close FILE;
+close LIST;
